87b0f52a86b81d46a7b55304742069ae5d55cc24
Ilia Lazebnik <Ilia.lazebnik@gmail.com>
2020-03-21 13:00:29 +0200

    Update resource_aws_opsworks_stack_test.go

--- aws/resource_aws_opsworks_stack_test.go
+++ aws/resource_aws_opsworks_stack_test.go
@@ -1039,7 +1039,7 @@ resource "aws_iam_role_policy" "opsworks_service" {
         "cloudwatch:GetMetricStatistics",
         "elasticloadbalancing:*",
         "rds:*",
-		"ecs:*"
+	"ecs:*"
       ],
       "Effect": "Allow",
       "Resource": [
