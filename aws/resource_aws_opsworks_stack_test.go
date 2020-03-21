880dc96edf338cf7db2ee05720880ae0c897d12b
DrFaust92 <ilia.lazebnik@gmail.com>
2020-03-21 12:47:32 +0200

    add ecs opsworks layer

--- aws/resource_aws_opsworks_stack_test.go
+++ aws/resource_aws_opsworks_stack_test.go
@@ -1038,7 +1038,8 @@ resource "aws_iam_role_policy" "opsworks_service" {
         "iam:PassRole",
         "cloudwatch:GetMetricStatistics",
         "elasticloadbalancing:*",
-        "rds:*"
+        "rds:*",
+		"ecs:*"
       ],
       "Effect": "Allow",
       "Resource": [
