7f3adca532372075bcb863112455da9d0a4c1cc6
DrFaust92 <ilia.lazebnik@gmail.com>
2020-06-26 23:57:57 +0300

    add arn

--- aws/resource_aws_ses_template_test.go
+++ aws/resource_aws_ses_template_test.go
@@ -6,7 +6,6 @@ import (
 	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/service/ses"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/acctest"
@@ -58,6 +57,7 @@ func TestAccAWSSesTemplate_Update(t *testing.T) {
 				Config: testAccCheckAwsSesTemplateResourceConfigBasic1(rName),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckSesTemplateExists(resourceName, &template),
+					testAccCheckResourceAttrRegionalARN(resourceName, "arn", "ses", fmt.Sprintf("template/%s", rName)),
 					resource.TestCheckResourceAttr(resourceName, "name", rName),
 					resource.TestCheckResourceAttr(resourceName, "html", "html"),
 					resource.TestCheckResourceAttr(resourceName, "subject", "subject"),
@@ -160,7 +160,7 @@ func testAccCheckSesTemplateDestroy(s *terraform.State) error {
 
 			gto, err := conn.GetTemplate(&input)
 			if err != nil {
-				if awsErr, ok := err.(awserr.Error); ok && (awsErr.Code() == "TemplateDoesNotExist") {
+				if isAWSErr(err, ses.ErrCodeTemplateDoesNotExistException, "") {
 					return nil
 				}
 				return resource.NonRetryableError(err)
